f20055d1858fc30334e1717874b32601f77f53b5
Sahish <sahishpandav@gmail.com>
2024-01-04 11:47:15 +0530

    Added carousel to Homepage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Homepage from "./components/Homepage";
 import Footer from "./components/Footer";
+
 function App() {
   return (
    <>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaInstagram } from "react-icons/fa";
 
 function Footer(props) {
     return (
@@ -8,19 +9,19 @@ function Footer(props) {
                     <div className="row g-5">
                         <div className="col-lg-3 col-md-6">
                             <h5 className="text-white mb-4">Company</h5>
-                            <a className="btn btn-link text-white-50" href="">About Us</a>
-                            <a className="btn btn-link text-white-50" href="">Contact Us</a>
-                            <a className="btn btn-link text-white-50" href="">Our Services</a>
-                            <a className="btn btn-link text-white-50" href="">Privacy Policy</a>
-                            <a className="btn btn-link text-white-50" href="">Terms & Condition</a>
+                            <a className="btn btn-link text-white-50" href="/">About Us</a>
+                            <a className="btn btn-link text-white-50" href="/">Contact Us</a>
+                            <a className="btn btn-link text-white-50" href="/">Our Services</a>
+                            <a className="btn btn-link text-white-50" href="/">Privacy Policy</a>
+                            <a className="btn btn-link text-white-50" href="/">Terms & Condition</a>
                         </div>
                         <div className="col-lg-3 col-md-6">
                             <h5 className="text-white mb-4">Quick Links</h5>
-                            <a className="btn btn-link text-white-50" href="">About Us</a>
-                            <a className="btn btn-link text-white-50" href="">Contact Us</a>
-                            <a className="btn btn-link text-white-50" href="">Our Services</a>
-                            <a className="btn btn-link text-white-50" href="">Privacy Policy</a>
-                            <a className="btn btn-link text-white-50" href="">Terms & Condition</a>
+                            <a className="btn btn-link text-white-50" href="/">About Us</a>
+                            <a className="btn btn-link text-white-50" href="/">Contact Us</a>
+                            <a className="btn btn-link text-white-50" href="/">Our Services</a>
+                            <a className="btn btn-link text-white-50" href="/">Privacy Policy</a>
+                            <a className="btn btn-link text-white-50" href="/">Terms & Condition</a>
                         </div>
                         <div className="col-lg-3 col-md-6">
                             <h5 className="text-white mb-4">Contact</h5>
@@ -28,10 +29,10 @@ function Footer(props) {
                             <p className="mb-2"><i className="fa fa-phone-alt me-3"></i>+91 987654321</p>
                             <p className="mb-2"><i className="fa fa-envelope me-3"></i>tif@gmail.com</p>
                             <div className="d-flex pt-2">
-                                <a className="btn btn-outline-light btn-social" href=""><i className="fab fa-twitter"></i></a>
-                                <a className="btn btn-outline-light btn-social" href=""><i className="fab fa-facebook-f"></i></a>
-                                <a className="btn btn-outline-light btn-social" href=""><i className="fab fa-youtube"></i></a>
-                                <a className="btn btn-outline-light btn-social" href=""><i className="fab fa-linkedin-in"></i></a>
+                                <a className="btn btn-outline-light btn-social" href="/"><i className="fab fa-twitter"></i></a>
+                                <a className="btn btn-outline-light btn-social" href="/"><FaInstagram /></a>
+                                <a className="btn btn-outline-light btn-social" href="/"><i className="fab fa-youtube"></i></a>
+                                <a className="btn btn-outline-light btn-social" href="/"><i className="fab fa-linkedin-in"></i></a>
                             </div>
                         </div>
                         <div className="col-lg-3 col-md-6">
@@ -47,13 +48,12 @@ function Footer(props) {
                 <div className="container">
                     <div className="copyright">
                         <div className="row">
-
                             <div className="col-md-6 text-center text-md-end">
                                 <div className="footer-menu">
-                                    <a href="">Home</a>
-                                    <a href="">Cookies</a>
-                                    <a href="">Help</a>
-                                    <a href="">FQAs</a>
+                                    <a href="/">Home</a>
+                                    <a href="/">Cookies</a>
+                                    <a href="/">Help</a>
+                                    <a href="/">FQAs</a>
                                 </div>
                             </div>
                         </div>
